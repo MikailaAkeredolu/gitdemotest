c18ebc4b54ebbb980f1fae8c2b775f5a488fb442
mikaila <mikaila@zipcodewilmington.com>
2018-09-26 16:03:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a sample redme to learn about this project
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I am going to push my homework to github by 9am" +
+                " tomorrow or else I will earn another infraction cos i am dumb as fuck!!!");
+    }
+}
