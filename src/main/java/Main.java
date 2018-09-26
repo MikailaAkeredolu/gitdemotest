44aa72048a5cb6c9769457a4257a1b755a070703
mikaila <mikaila@zipcodewilmington.com>
2018-09-26 16:06:11 -0400

    updated code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I am going to push my homework to github by 9am" +
-                " tomorrow or else I will earn another infraction cos i am dumb as fuck!!!");
+                " tomorrow ");
     }
 }
